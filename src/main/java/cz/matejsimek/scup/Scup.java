76934d84ef398387813d6d1bbfa061e9f232cb7f
Matej Simek <email@matejsimek.cz>
2012-06-14 00:55:08 +0200

    Tray icon implementation

--- src/main/java/cz/matejsimek/scup/Scup.java
+++ src/main/java/cz/matejsimek/scup/Scup.java
@@ -1,12 +1,23 @@
 package cz.matejsimek.scup;
 
+import java.awt.AWTException;
+import java.awt.CheckboxMenuItem;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
 import java.awt.Toolkit;
+import java.awt.TrayIcon;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.FlavorEvent;
 import java.awt.datatransfer.FlavorListener;
+import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -23,20 +34,25 @@ public class Scup {
 
     private static Clipboard clipboard;
     private static FileUpload fileupload;
-
+    private static SystemTray tray;
+    private static TrayIcon trayIcon;
     // User configuration
     private static String FTP_SERVER, FTP_USERNAME, FTP_PASSWORD, FTP_DIRECTORY, URL;
+    // Runtime configuration
+    private static boolean isUploadEnabled = true;
 
     public static void main(String[] args) throws InterruptedException, IOException {
 	File configFile = new File("config.properties");
-	if(configFile.exists()){
+	if (configFile.exists()) {
 	    Properties config = new Properties();
 	    config.load(new FileInputStream(configFile));
 	    readConfiguration(config);
-	} else{
+	} else {
 	    System.err.println("Configuration file config.properties doesn't exist, please create one.");
 	    System.exit(1);
 	}
+
+	initTray();
 
 	clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 	clipboard.addFlavorListener(new ClipboardChangeListener());
@@ -49,7 +65,59 @@ public class Scup {
 	}
     }
 
-    static private void readConfiguration(Properties config){
+    static private void initTray() {
+	if (SystemTray.isSupported()) {
+	    tray = SystemTray.getSystemTray();
+	    trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage("icon.png"), "Scup");
+	    //trayIcon.setImageAutoSize(true);
+
+	    try {
+		tray.add(trayIcon);
+	    } catch (AWTException e) {
+		System.out.println("TrayIcon could not be added.");
+		System.exit(1);
+	    }
+
+	    PopupMenu popup = new PopupMenu();
+	    final CheckboxMenuItem uploadEnabledCheckBox = new CheckboxMenuItem("Upload enabled");
+	    MenuItem exitItem = new MenuItem("Exit");
+	    popup.add(uploadEnabledCheckBox);
+	    popup.addSeparator();
+	    popup.add(exitItem);
+
+	    trayIcon.setPopupMenu(popup);
+
+	    uploadEnabledCheckBox.setState(isUploadEnabled);
+
+	    // Add listener to uploadEnabledCheckBox.
+	    uploadEnabledCheckBox.addItemListener(new ItemListener() {
+		public void itemStateChanged(ItemEvent e) {
+		    int chxbx = e.getStateChange();
+		    if (chxbx == ItemEvent.SELECTED) {
+			isUploadEnabled = true;
+			uploadEnabledCheckBox.setLabel("Upload enabled");
+		    } else {
+			isUploadEnabled = false;
+			uploadEnabledCheckBox.setLabel("Upload disabled");
+		    }
+		}
+	    });
+
+	    // Add listener to exitItem.
+	    exitItem.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    tray.remove(trayIcon);
+		    System.exit(0);
+		}
+	    });
+
+	    trayIcon.displayMessage("Scup", "I am here to serve", TrayIcon.MessageType.NONE);
+	} else {
+	    System.err.println("SystemTray is not supported");
+	}
+    }
+
+    static private void readConfiguration(Properties config) {
 	FTP_SERVER = config.getProperty("FTP_SERVER");
 	FTP_USERNAME = config.getProperty("FTP_USERNAME", "anonymous");
 	FTP_PASSWORD = config.getProperty("FTP_PASSWORD", "");
@@ -60,21 +128,42 @@ public class Scup {
     static void processImage(BufferedImage img) {
 	System.out.println("Processing image...");
 	System.out.println("Image: " + img.getWidth() + "x" + img.getHeight());
-	// TODO crop image
-	File imageFile = saveImageToFile(img);
-	// Transer image to FTP
-	System.out.println("Uploading image to FTP server...");
-	if(fileupload.uploadFile(imageFile, imageFile.getName())){
-	    System.out.println("Upload done");
-	} else{
-	    System.err.println("Upload failed");
-	}
-	// Generate URL
-	String url = (URL.endsWith("/") ? URL : URL + "/") + imageFile.getName();
-	System.out.println(url);
-	// Copy URL to clipboard
 
-	// Notify me about it
+	// TODO crop image
+
+	File imageFile = saveImageToFile(img);
+
+	if (isUploadEnabled) {
+	    // Transer image to FTP
+	    System.out.println("Uploading image to FTP server...");
+	    if (fileupload.uploadFile(imageFile, imageFile.getName())) {
+		System.out.println("Upload done");
+		// Generate URL
+		String url = (URL.endsWith("/") ? URL : URL + "/") + imageFile.getName();
+		System.out.println(url);
+		// Copy URL to clipboard
+		try {
+		    clipboard.setContents(new StringSelection(url), null);
+		} catch (IllegalStateException e) {
+		    System.err.println("Can't set clipboard, sorry!");
+		}
+		// Notify me about it
+		trayIcon.displayMessage("Image uploaded", url, TrayIcon.MessageType.INFO);
+	    } else {
+		System.err.println("Upload failed");
+		trayIcon.displayMessage("Upload failed", "I can not serve, sorry", TrayIcon.MessageType.ERROR);
+	    }
+	} else {
+	    // Copy URL to clipboard
+	    try {
+		clipboard.setContents(new StringSelection(imageFile.getAbsolutePath()), null);
+	    } catch (IllegalStateException e) {
+		System.err.println("Can't set clipboard, sorry!");
+	    }
+	    // Notify me about it
+	    trayIcon.displayMessage("Image saved", imageFile.getAbsolutePath(), TrayIcon.MessageType.INFO);
+	}
+
     }
 
     static void processFiles(List<File> files) {
@@ -83,6 +172,8 @@ public class Scup {
 	    System.out.println("File " + i + ": " + files.get(i).getName());
 	}
 	System.err.println("Not supported feature yet, stay tuned!");
+
+
     }
 
     static class ClipboardChangeListener implements FlavorListener {
